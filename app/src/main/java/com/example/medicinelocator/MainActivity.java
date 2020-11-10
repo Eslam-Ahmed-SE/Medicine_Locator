023fd110ce800f158358ad40edd002b5d70dad49
ryzenpc <2751998>
2020-11-10 14:23:58 +0200

    Added hidden search bar that show on scroll

--- app/src/main/java/com/example/medicinelocator/MainActivity.java
+++ app/src/main/java/com/example/medicinelocator/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.medicinelocator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.facebook.shimmer.ShimmerFrameLayout;
 
@@ -16,7 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView medicineList;
     private ConstraintLayout item_container;
+    private ConstraintLayout searchContainer;
     private ShimmerFrameLayout medicines_loading;
+    private NestedScrollView scrollContainer;
+    private EditText hidden_search_bar;
+
+
+    int positionToScroll=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,28 @@ public class MainActivity extends AppCompatActivity {
 
         FillMedicineListTask medicineListTask = new FillMedicineListTask(medicineList, this);
         medicineListTask.execute();
+
+        final Rect scrollBounds = new Rect();
+        scrollContainer.getHitRect(scrollBounds);
+        scrollContainer.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+//                Log.i("scroll", "int scrollX: " + scrollX + " int scrollY: " + scrollY + " int oldScrollX: " + oldScrollX + " int oldScrollY: " + oldScrollY);
+                Rect scrollBounds = new Rect();
+                scrollContainer.getHitRect(scrollBounds);
+                if (searchContainer.getLocalVisibleRect(scrollBounds)) {
+                    Log.i("scroll", "true");
+                    hidden_search_bar.setVisibility(View.GONE);
+
+                } else {
+                    Log.i("scroll", "false");
+                    hidden_search_bar.setVisibility(View.VISIBLE);
+                }
+
+
+            }
+        });
+
 //        if(medicineListTask.getStatus() == AsyncTask.Status.PENDING){
 //            // My AsyncTask has not started yet
 //        }
@@ -44,7 +78,10 @@ public class MainActivity extends AppCompatActivity {
     private void initComponents() {
         medicineList = findViewById(R.id.medList);
         item_container = findViewById(R.id.recent_container);
+        searchContainer = findViewById(R.id.searchContainer);
         medicines_loading = findViewById(R.id.medicines_loading);
+        scrollContainer = findViewById(R.id.scrollContainer);
+        hidden_search_bar = findViewById(R.id.hiddenSearchBar);
 
         medicineList.setLayoutManager(new LinearLayoutManager(this));
         medicineList.setItemAnimator(new DefaultItemAnimator());
